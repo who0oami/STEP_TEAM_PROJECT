6ff991ae150b75964338b8dc4f45b5e64d29f082
luha <gracefield0814@gmail.com>
2025-12-14 15:46:49 +0900

    seed 수정, 웹 정보 찾기 페이지 수정

--- step_app/lib/model/seed_category_color.dart
+++ step_app/lib/model/seed_category_color.dart
@@ -1,9 +1,48 @@
-final List<String> seedCategoryColors = [
-  'BLACK',
-  'WHITE',
-  'GRAY',
-  'RED',
-  'BLUE',
-  'GREEN',
-  'BROWN',
+import 'package:step_app/model/category_color.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SeedCategoryColor {
+  static Future<void> insertSeed(Database db) async {
+    for (final c in seedCategoryColors) {
+      await db.insert(
+        'categorycolor',
+        {
+          'category_color_id': c.category_color_id,
+          'category_color_name': c.category_color_name,
+        },
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    }
+  }
+}
+
+final List<Category_color> seedCategoryColors = [
+  Category_color(
+    category_color_id: 1,
+    category_color_name: 'BLACK',
+  ),
+  Category_color(
+    category_color_id: 2,
+    category_color_name: 'WHITE',
+  ),
+  Category_color(
+    category_color_id: 3,
+    category_color_name: 'GRAY',
+  ),
+  Category_color(
+    category_color_id: 4,
+    category_color_name: 'RED',
+  ),
+  Category_color(
+    category_color_id: 5,
+    category_color_name: 'BLUE',
+  ),
+  Category_color(
+    category_color_id: 6,
+    category_color_name: 'GREEN',
+  ),
+  Category_color(
+    category_color_id: 7,
+    category_color_name: 'BROWN',
+  ),
 ];
