d1f5da93bda3f6d7a4a1098e3c57fe0e2d094482
dusigi <jangdusigi@naver.com>
2025-12-15 11:42:50 +0900

    수정 (지도, 결재 페이지 등 ) 중간 저장

--- step_app/lib/main.dart
+++ step_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:step_app/view/app/home.dart';
+import 'package:step_app/view/app/purchase_page.dart';
+import 'package:step_app/view/web/purchase_order.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home(),
+      home: PurchaseOrder(),
     );
   }
 }
